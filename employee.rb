6059a1ce3a6350881eb9bbef7d685124907907b9
Olena+Sean <olena+sean@appacademy.io>
2013-05-21 10:41:41 -0700

    add manager bonus for one level of employees

--- employee.rb
+++ employee.rb
@@ -5,26 +5,9 @@ class Employee
     @boss = []
   end
 
-  # def sub_employee_salary(employee)
-  #   visited = []
-  #   sub_employees = self.employees # get all employees for current
-  #   while sub_employees # while we have employees still
-  #     employee = sub_employees.pop # grab one employee
-  #     employees += employee.employees # add her employees to stack
-  # end
-
   def calculate_bonus(multiplier = 1)
     raise "Please set salary for #{self.name}" if salary.nil?
-    case self.class.is_a?
-    when Employee
-      puts "or here"
-      bonus = salary * multiplier
-    when Manager
-      puts "I'm here :)"
-      self.employees.map do |employee|
-        employee.salary #+ employee.calculate_bonus
-      end.inject(&:+)
-    end
+    bonus = salary * multiplier
   rescue => e
     puts "#{e.message}"
     puts "There is no salary set for this employee..."
@@ -43,6 +26,12 @@ class Manager < Employee
     @employees << employee unless employees.include?(employee)
     employee.boss << self
   end
+
+  def calculate_bonus(multiplier = 1)
+    self.employees.map do |employee|
+      employee.salary
+    end.inject(&:+) * multiplier
+  end
 end
 
 sean = Manager.new('Sean')
@@ -52,8 +41,8 @@ fred = Employee.new("Fred")
 sean.add_employee(olena)
 sean.add_employee(fred)
 olena.add_employee(fred)
-fred.salary = 10000
-olena.salary = 10000
+fred.salary = 1
+olena.salary = 1000
 sean.salary = 20000
 
 p sean.calculate_bonus(1)
